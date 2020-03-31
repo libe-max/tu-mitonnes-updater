6a70389c4d8ac161756584f05e7c8519089a3dcc
Maxime Fabas <maxime.fabas@pm.me>
2020-03-31 17:20:12 +0200

    start working on googleapis

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
+const { google } = require('googleapis')
+const credentials = require('./secret-credentials.json')
 
-const initUrl = 'https://www.liberation.fr/mapi/sections/99773/contents/?format=json'
+const client = new google.auth.JWT(credentials.client_email, null, credentials.private_key, [])
 
-recurseFetchApiPage()
+// const initUrl = 'https://www.liberation.fr/mapi/sections/99773/contents/?format=json'
 
-async function recurseFetchApiPage (currUrl = initUrl, results = [], page = 0) {
-  console.log(`loading page ${page}...\n`)
-  const data = await fetchApiPage(currUrl)
-  const newResults = [...results, ...data.results]
-  if (data.next && data.results.length) return recurseFetchApiPage(data.next, newResults, page + 1)
-  console.log(`done.\n`)
-  console.log(`Found ${newResults.length} results`)
-  return newResults
-}
+// recurseFetchApiPage()
 
-async function fetchApiPage (url) {
-  try {
-    const res = await fetch(url)
-    const data = await res.json()
-    return data
-  } catch (e) {
-    console.log(e)
-    process.exit(1)
-  }
-}
+// async function recurseFetchApiPage (currUrl = initUrl, results = [], page = 0) {
+//   console.log(`loading page ${page}...\n`)
+//   const data = await fetchApiPage(currUrl)
+//   const newResults = [...results, ...data.results]
+//   if (data.next && data.results.length) return recurseFetchApiPage(data.next, newResults, page + 1)
+//   console.log(`done.\n`)
+//   console.log(`Found ${newResults.length} results`)
+//   return newResults
+// }
+
+// async function fetchApiPage (url) {
+//   try {
+//     const res = await fetch(url)
+//     const data = await res.json()
+//     return data
+//   } catch (e) {
+//     console.log(e)
+//     process.exit(1)
+//   }
+// }
