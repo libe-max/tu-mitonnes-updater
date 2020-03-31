808d1a24c3916c2da81685ed7b15eb0a13747da0
Maxime Fabas <maxime.fabas@pm.me>
2020-03-31 11:34:27 +0200

    first block of logic fetching data from Libé API

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
 
-console.log(fetch)
+const initUrl = 'https://www.liberation.fr/mapi/sections/99773/contents/?format=json'
+
+recurseFetchApiPage()
+
+async function recurseFetchApiPage (currUrl = initUrl, results = [], page = 0) {
+  console.log(`loading page ${page}...\n`)
+  const data = await fetchApiPage(currUrl)
+  const newResults = [...results, ...data.results]
+  if (data.next && data.results.length) return recurseFetchApiPage(data.next, newResults, page + 1)
+  console.log(`done.\n`)
+  console.log(`Found ${newResults.length} results`)
+  return newResults
+}
+
+async function fetchApiPage (url) {
+  try {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+}
