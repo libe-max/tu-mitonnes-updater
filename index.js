f8219fe1d4cb07d3d320a6bd5e7e1b7d69fe9699
libe-max <libe.maxime.fabas@gmail.com>
2020-04-02 17:18:06 +0200

    works as a cron task

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 const fetch = require('node-fetch')
-const { google } = require('googleapis')
 const moment = require('moment')
+const cron = require('node-cron')
+const { google } = require('googleapis')
 const credentials = require('./secret-credentials.json')
 
 const spreadsheetId = '1TnhpEyk-2sofVWtErqvlt_5QFLHUHmJWUCql8NLxrwo'
 const libeApiBaseUrl = 'https://www.liberation.fr/mapi/sections/99773/contents/?format=json'
 
 doTheJob()
+cron.schedule('0 0,15,30,45 * * * *', doTheJob)
 
 /*
  *
@@ -18,6 +20,10 @@ doTheJob()
 
 async function doTheJob () {
   try {
+    console.log('\n')
+    console.log('START BACKUP')
+    console.log(moment().format())
+    console.log('==============')
     console.log('Create sheets Api client...')
     const sheetsApi = await createSheetsApiClient()
     console.log('done.\n')
@@ -25,7 +31,8 @@ async function doTheJob () {
     console.log('Get raw sheet data...\n')
     const initRawSheetData = await getRawSheetData(sheetsApi)
     const initSheetData = makeObjectsFromRawSheetData(initRawSheetData)
-    console.log(initSheetData)
+    console.log(`${initSheetData.length} in sheet, latest one is:\n`)
+    console.log(initSheetData.slice(0, 1))
     console.log('\n')
     
     console.log('Fetch new stuff from Libération API...\n')
@@ -33,13 +40,11 @@ async function doTheJob () {
     console.log(fetchedFromLibeApi)
     console.log('\n')
 
-    if (!fetchedFromLibeApi.length) {
-      console.log('Nothing new to save. Exiting now.')
-      return process.exit(0)
-    }
+    if (!fetchedFromLibeApi.length) return console.log('Nothing new to save. See you.')
 
     const newRawSheetData = await prepareNewRawSheetData(fetchedFromLibeApi, initSheetData)
-    console.log(newRawSheetData)
+    console.log(`${newRawSheetData.length} in new sheet, latest one is:\n`)
+    console.log(newRawSheetData.slice(0, 1))
     console.log('\n')
 
     console.log('Saving new data...')
@@ -53,11 +58,11 @@ async function doTheJob () {
     fs.writeFileSync(backupFileOutputPath, jsonInitSheetData, 'utf8')
     console.log('done.\n')
 
-    console.log('Bye now.')
+    console.log('See you.')
 
   } catch (err) {
     console.log(err)
-    return process.exit(1)
+    return
   }
 }
 
@@ -127,7 +132,7 @@ function makeObjectsFromRawSheetData (rawData = []) {
 
 async function fetchNewEntriesInLibeApi (initEntries = [], currUrl = libeApiBaseUrl, results = [], page = 0) {
   console.log(`Loading API page ${page}`)
-  const pageData = await fetchApiPage(currUrl)
+  const pageData = await fetchLibeApiPage(currUrl)
   const pageResults = pageData.results
   const pageResultsThatAreNew = pageResults.filter(entry => {
     const entryId = entry.id.toString()
@@ -141,7 +146,7 @@ async function fetchNewEntriesInLibeApi (initEntries = [], currUrl = libeApiBase
     })
     return flattenedEntry
   })
-  console.log(`Found ${pageResults.length} entries, and ${pageResultsThatAreNew.length} new ones.\n`)
+  console.log(`Found ${pageResults.length} entries, including ${pageResultsThatAreNew.length} new ones.\n`)
   const newResults = [...results, ...pageResultsThatAreNew]
   if (!pageData.next || !pageResultsThatAreNew.length) return newResults
   return fetchNewEntriesInLibeApi(initEntries, pageData.next, newResults, page + 1)
@@ -149,11 +154,11 @@ async function fetchNewEntriesInLibeApi (initEntries = [], currUrl = libeApiBase
 
 /*
  *
- * FETCH API PAGE
+ * FETCH LIBÉ API PAGE
  *
  */
 
-async function fetchApiPage (url) {
+async function fetchLibeApiPage (url) {
   console.log(`Fetching ${url}...`)
   const res = await fetch(url)
   const data = await res.json()
@@ -170,29 +175,30 @@ async function prepareNewRawSheetData (newData = [], initData = []) {
   const mergedData = [...newData, ...initData]
   const sortedMergedData = [...mergedData].sort((a, b) => a.modified_at - b.modified_at)
   const tableData = mergedData.map(article => ([
-    article.title,
-    article.ingredients,
-    article.dish_type,
-    article.season,
-    article.subtitle,
-    article.url,
-    article.id,
-    article.slug,
-    article.access,
-    article.type,
-    article.typology,
-    article.primary_section,
-    article.primary_section_title,
-    article.call_photo_url,
-    article.call_photo_credits,
-    article.call_photo_caption,
-    article.call_photo_width,
-    article.call_photo_height,
-    article.call_photo_format,
-    article.publication_date,
-    article.modified_at,
-    article.updating_date,
-    article.live_flux
+    '0',
+    article.title || '',
+    article.ingredients || '',
+    article.dish_type || '',
+    article.season || '',
+    article.subtitle || '',
+    article.url || '',
+    article.id || '',
+    article.slug || '',
+    article.access || '',
+    article.type || '',
+    article.typology || '',
+    article.primary_section || '',
+    article.primary_section_title || '',
+    article.call_photo_url || '',
+    article.call_photo_credits || '',
+    article.call_photo_caption || '',
+    article.call_photo_width || '',
+    article.call_photo_height || '',
+    article.call_photo_format || '',
+    article.publication_date || '',
+    article.modified_at || '',
+    article.updating_date || '',
+    article.live_flux || ''
   ]))
   return tableData
 }
